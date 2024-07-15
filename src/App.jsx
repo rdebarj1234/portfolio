d385c976ef988453801ba05b44fb53c7e023d009
REYA DEBARAJ <131584554+rdebarj1234@users.noreply.github.com>
2024-07-15 17:14:48 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Componenets/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componenets/Navbar.jsx
@@ -0,0 +1,27 @@
+const Navbar = () => {
+  return (
+    <div>
+      <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
+        <div>
+          <div>
+            <img src="" alt="" />
+            <h1>Reya Devraj</h1>
+            <p>Web Developer</p>
+          </div>
+          <div>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Portfolio</li>
+              <li>Experience</li>
+              <li>Contacts</li>
+            </ul>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
